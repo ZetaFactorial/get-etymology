c2627c654377f7f98f1634188dd56a8d7c0b7093
Zeta Factorial <alexbolgov49@gmail.com>
2022-09-27 10:51:35 +0300

    Add async parsing

--- README.md
+++ README.md
@@ -3,7 +3,5 @@ Wiktionary scraper for getting the etymology of a word.
 
 ## Example usage
 ```python
->>> from get_etymology import scrap_wiktionary
->>> scrap_wiktionary("ёфикация") 
-'Происходит от ё + -фикация. Cлово придумал В. Т. Чумаков.\n'
+...
 ```
--- get_etymology/get_etymology.py
+++ get_etymology/get_etymology.py
@@ -1,28 +1,51 @@
 """Wikipedia won't lie!"""
 
-import requests
+import aiohttp
+import asyncio
 from bs4 import BeautifulSoup
 import urllib
 import logging
 
+logging.basicConfig(level=logging.ERROR)
 log = logging.getLogger(__name__)
 
+async def fetch(session: aiohttp.ClientSession, url: str) -> str:
+    log.info(f"Fetching from {url}")
+    async with session.get(url) as response:
+        response.raise_for_status()
+        return await response.text()
+    
 def wiktionary_url(word: str) -> str:
     word = urllib.parse.quote(word.encode('utf-8'))
     return f"https://ru.wiktionary.org/wiki/{word}"
 
-def scrap_wiktionary(word: str) -> str | None:
-    page = requests.get(wiktionary_url(word))
-    if page.status_code != 200:
-        log.error(f'Error {page.status_code}')
-        return
-    soup = BeautifulSoup(page.text, "html.parser")
+def get_etymology_on_wiktionary_page(page: str) -> str:
+    soup = BeautifulSoup(page, "html.parser")
     etymology = soup.find(id="Этимология").find_next('p').text
     return etymology
 
-def main() -> None:
-    etymology = scrap_wiktionary(input())
-    print(etymology)
+async def get_etymology(session: aiohttp.ClientSession, word: str) -> str:
+    url = wiktionary_url(word)
+    try:
+        page = await fetch(session, url)
+    except (aiohttp.ClientError) as exc:
+        log.error(f'Cannot get word {word} from Wiktionary: Error {exc.status}')
+        return ''
+    else:
+        log.info(f'Got etymology for {word}')
+        etymology = get_etymology_on_wiktionary_page(page)
+        print(f'*Этимология слова {word}.*', etymology)
+        return etymology
+
+async def scrap(words: list[str]) -> list:
+    async with aiohttp.ClientSession() as session:
+        tasks = [get_etymology(session, word) for word in words]
+        return await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
-    main()
+    
+    import platform
+    if platform.system()=='Windows': # i hate windows
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+        
+    asyncio.run(scrap(['база', 'фундамент', 'базис']))
