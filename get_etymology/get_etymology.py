c1da03c59b288babb3f706e0e9ef41cf1302b173
Zeta Factorial <alexbolgov49@gmail.com>
2022-09-29 14:13:48 +0300

    Use async file writing

--- get_etymology/get_etymology.py
+++ get_etymology/get_etymology.py
@@ -1,16 +1,18 @@
 """Wikipedia won't lie!"""
 
-import aiohttp
 import asyncio
-from bs4 import BeautifulSoup
+import aiofiles
+import aiohttp
 import urllib
+from bs4 import BeautifulSoup
 import logging
+import pathlib
 
 import platform
 if platform.system()=='Windows': # i hate windows
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
 async def fetch(session: aiohttp.ClientSession, url: str) -> str:
@@ -20,7 +22,7 @@ async def fetch(session: aiohttp.ClientSession, url: str) -> str:
         return await response.text()
     
 def wiktionary_url(word: str) -> str:
-    word = urllib.parse.quote(word.encode('utf-8'))
+    word = urllib.parse.quote(word.encode('utf-8')).lower()
     return f"https://ru.wiktionary.org/wiki/{word}"
 
 def get_etymology_on_wiktionary_page(page: str) -> str:
@@ -28,26 +30,32 @@ def get_etymology_on_wiktionary_page(page: str) -> str:
     etymology = soup.find(id="Этимология").find_next('p').text
     return etymology
 
-async def get_etymology(session: aiohttp.ClientSession, word: str) -> str:
+async def get_etymology(session: aiohttp.ClientSession, word: str) -> None:
     url = wiktionary_url(word)
     try:
         page = await fetch(session, url)
     except (aiohttp.ClientError) as exc:
         log.error(f'Cannot get word {word} from Wiktionary: Error {exc.status}')
-        return ''
     else:
         log.info(f'Got etymology for {word}')
         etymology = get_etymology_on_wiktionary_page(page)
-        print(f'*Этимология слова {word}.*', etymology)
-        return etymology
+        async with aiofiles.open(pathlib.Path(__file__).parent/'outp.md', 'a', encoding='utf-8') as outp:
+            log.info('Opening outp.md')
+            await outp.write(f'*Этимология слова {word}.*\n{etymology}\n')
+            log.info('Closing outp.md')
 
-async def scrap(words: list[str]) -> list:
+async def scrap(words: set[str]) -> list:
     async with aiohttp.ClientSession() as session:
-        tasks = [get_etymology(session, word) for word in words]
+        log.info(f'Scraping {words}')
+        tasks = set(get_etymology(session, word) for word in words)
         return await asyncio.gather(*tasks)
 
-def main() -> None:
-    asyncio.run(scrap(input().split()))
+def get_words() -> set[str]:
+    words: set[str] = set()
+    with open(pathlib.Path(__file__).parent/'inp.md', 'r', encoding='utf8') as inp:
+        for line in inp:
+            words.update(word for word in line.split())
+    return words
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(scrap(get_words()))
